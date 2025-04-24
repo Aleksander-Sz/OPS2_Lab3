3ffb93154d46bc1c3a0a8c34590203da7b30c99a
327255 szydlowski aleksander <szydlowskia@p21809.mini.pw.edu.pl>
2025-04-24 16:27:12 +0200

    stage 3

--- sop-race.c
+++ sop-race.c
@@ -104,26 +104,31 @@ void child_work(sync_data* shared_1)
             printf("%d waf waf (finished race))\n", my_pid);
             return;
         }
-        else if (racetrack[new_pos] == 0)
-        {
-            if (position > 0)
-            {
-                pthread_mutex_lock(&racetrack_mutex[position]);
-                racetrack[position] = 0;
-                pthread_mutex_unlock(&racetrack_mutex[position]);
-            }
-            // pthread_mutex_lock(&racetrack_mutex[220]);
-            // pthread_mutex_unlock(&racetrack_mutex[220]);
-            position = new_pos;
-            pthread_mutex_lock(&racetrack_mutex[position]);
-            racetrack[position] = my_pid;
-            shared_1->direction[position] = direction;
-            pthread_mutex_unlock(&racetrack_mutex[position]);
-            printf("%d waf waf (new position = %d)\n", my_pid, position);
-        }
         else
         {
-            printf("%d waf waf (the field is occupied)\n", my_pid);
+            pthread_mutex_lock(&racetrack_mutex[position]);
+            if (racetrack[new_pos] == 0)
+            {
+                if (position > 0)
+                {
+                    // pthread_mutex_lock(&racetrack_mutex[position]);
+                    racetrack[position] = 0;
+                    pthread_mutex_unlock(&racetrack_mutex[position]);
+                }
+                // pthread_mutex_lock(&racetrack_mutex[220]);
+                // pthread_mutex_unlock(&racetrack_mutex[220]);
+                position = new_pos;
+                pthread_mutex_lock(&racetrack_mutex[position]);
+                racetrack[position] = my_pid;
+                shared_1->direction[position] = direction;
+                pthread_mutex_unlock(&racetrack_mutex[position]);
+                printf("%d waf waf (new position = %d)\n", my_pid, position);
+            }
+            else
+            {
+                pthread_mutex_unlock(&racetrack_mutex[position]);
+                printf("%d waf waf (the field is occupied)\n", my_pid);
+            }
         }
         move_count++;
     }
